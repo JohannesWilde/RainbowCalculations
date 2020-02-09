9506da7fb96b2821aaa59101f31d98bea0d93807
Johannes Wilde <git@jowil.de>
2020-02-09 17:09:06 +0100

    Add helper.

--- UnitCircleHelpers.py
+++ UnitCircleHelpers.py
@@ -6,3 +6,9 @@ from Point import Point2D
 
 def unitCirclePointFromAngle(angle):
     return Point2D(x=cos(angle.radians), y=sin(angle.radians))
+
+def otherIntersectionOf(pointFrom, direction):
+    '''pointFrom must lie on the unit cirecle and direction must be normalized.'''
+    tempVal = 2 * (pointFrom.x * direction.x + pointFrom.y * direction.y)
+    return Point2D(x=(pointFrom.x - direction.x * tempVal),
+                   y=(pointFrom.y - direction.y * tempVal))
