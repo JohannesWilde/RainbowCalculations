b8bb5f0a64cec5eaed5cb3b63d5db9701d8e3d55
Johannes Wilde <git@jowil.de>
2020-02-09 16:14:29 +0100

    Make TypeChecker base.

--- TypeHelpers.py
+++ TypeHelpers.py
@@ -2,5 +2,15 @@
 
 NumberTypes = (int, float)
 
-def checkNumber(value):
-    return isinstance(value, NumberTypes)
+class TypeChecker(object):
+
+    @staticmethod
+    def checkType(expectedType, value, exceptionMessage):
+        if not isinstance(value, expectedType):
+            raise TypeError \
+                ('Incompatible type supplied [{type}]: {message}.'.format(type=type(value), message=exceptionMessage))
+        return
+
+    @staticmethod
+    def checkNumber(value):
+        return isinstance(value, NumberTypes)
