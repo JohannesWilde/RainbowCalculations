41a3652c0d2bbfa11dc529dd9e9aecf27f4be904
Johannes Wilde <git@jowil.de>
2020-02-09 16:23:49 +0100

    Everything regarding beta.

--- RaindropCalculations.py
+++ RaindropCalculations.py
@@ -9,9 +9,54 @@ class RaindropCalculations(object):
     def __init__(self, refractiveIndexOuter, refractiveIndexInner, incidenceHeight):
         self.refractiveIndexOuter = refractiveIndexOuter
         self.refractiveIndexInner = refractiveIndexInner
-        self.incidenceHeigth = incidenceHeight
+        self.incidenceHeight = incidenceHeight
         return
 
-    def getIncidencePoint(self):
-        alpha0 = Angle(radians=asin(self.incidenceHeigth))
-        return unitCirclePointFromAngle(alpha0)
+    @property
+    def n0(self):
+        return self.refractiveIndexOuter
+
+    @property
+    def n1(self):
+        return self.refractiveIndexInner
+
+    @property
+    def h0(self):
+        return self.incidenceHeight
+
+    @property
+    def alpha0(self):
+        return Angle(radians=asin(self.h0))
+
+    # beta
+    @property
+    def pointBeta(self):
+        '''Incidence.'''
+        return unitCirclePointFromAngle(self.alpha0)
+
+    @property
+    def beta0(self):
+        return self.alpha0
+
+    @property
+    def beta1(self):
+        return Angle(radians=asin(self.n0/self.n1*self.h0))
+
+    @property
+    def epsilon0(self):
+        return (self.beta0 - self.beta1)
+
+
+    # gamma
+    @property
+    def pointGamma(self):
+        '''Reflection.'''
+
+        return
+
+    # delta
+    @property
+    def pointDelta(self):
+        '''Emergence.'''
+
+        return
