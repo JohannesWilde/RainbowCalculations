e5b4e6ea06da0908a73d6407a5cef5e05d4e743c
Johannes Wilde <git@jowil.de>
2020-02-09 14:40:08 +0100

    Show incident ray.

--- RainbowSimulation.py
+++ RainbowSimulation.py
@@ -7,10 +7,26 @@
 from matplotlib import pyplot as plt
 from math import sin, cos, tan, asin, atan
 
+from Angle import Angle
+from UnitCircleHelpers import unitCirclePointFromAngle
+
+class ObjectZorder(object):
+    # bigger values are to the front
+    Raindrop = 1
+    MeetingPoints = 2
+    Lightray = 3
+
+class ObjectColor(object):
+    MeetingPoints = (0.,.8,0.)
+    Lightray = (.8,0.,0.)
+
 
 if __name__ == '__main__':
     refractiveIndexOuter = 1.
     refractiveIndexInner = 1.5
+
+    alpha0 = Angle(degrees=40)
+
 
     # get plot
     axis = plt.gca()
@@ -21,16 +37,18 @@ if __name__ == '__main__':
     axis.set_aspect('equal')
 
     # draw sphere
-    colorValue = 5. / (1. + refractiveIndexInner / refractiveIndexOuter)
+    colorValue = 2. / (1. + refractiveIndexInner / refractiveIndexOuter)
     sphereBackgroundColor = (colorValue, colorValue, colorValue)
 
-    raindrop = plt.Circle((0, 0), 1, color=sphereBackgroundColor, fill=True)
+    raindrop = plt.Circle((0, 0), 1, color=sphereBackgroundColor, fill=True, zorder=ObjectZorder.Raindrop)
 
     axis.add_artist(raindrop)
 
-    # show light ray
+    # show light ray - incident
+    pointBeta = unitCirclePointFromAngle(alpha0)
+    plt.scatter(pointBeta.x, pointBeta.y, marker='.', zorder=ObjectZorder.MeetingPoints, color=ObjectColor.MeetingPoints)
 
-
+    plt.plot((2, pointBeta.x), (pointBeta.y, pointBeta.y), color=ObjectColor.Lightray)
 
     # make plot visible
     plt.show()
